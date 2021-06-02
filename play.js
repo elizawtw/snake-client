3f78289b117d7b1b95b64c503566567d45dbc2f4
elizawtw <twew02@gmail.com>
2021-06-02 19:01:45 +0000

    added setupInput and handleUserInput functions

--- play.js
+++ play.js
@@ -1,7 +1,26 @@
 const net = require("net");
+const { stdin } = require("process");
 const connect = require('./client');
 // establishes a connection with the game server
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+  
+};
+const handleUserInput = function (data) {
+  // your code here
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+stdin.on("data", handleUserInput);
+
 
 
 console.log("Connecting ...");
 connect();
+setupInput();
