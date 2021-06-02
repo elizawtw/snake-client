7a8469b1049317cc1c1d540e0ff9dd1c058ce2ee
elizawtw <twew02@gmail.com>
2021-06-02 19:12:19 +0000

    transferred setupInput and handleUserInput functions to input.js

---
+++ input.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+  
+  
+};
+
+const handleUserInput = function (data) {
+  // your code here
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = {setupInput, handleUserInput};
--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 const net = require("net");
 const { stdin } = require("process");
 const connect = require('./client');
+const {setupInput} = require('./input');
 // establishes a connection with the game server
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-  
-};
-const handleUserInput = function (data) {
-  // your code here
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-
-stdin.on("data", handleUserInput);
 
 
 
